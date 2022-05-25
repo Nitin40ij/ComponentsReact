c871499c6ac2c83e4a0c30d86d59df89881d2d37
Nitin Srivastav <nitin.srivastav_cs19@gla.ac.in>
2022-05-26 00:19:00 +0530

    First commit

---
+++ src/Create.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Fun extends React.Component{
+ render(){
+     return(
+         <h1 style={{color:"blue"}}>Hello from React Class Component</h1>
+     )
+ }
+}
+export default Fun;
---
+++ src/FuncComp.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const fun=()=>{
+    return <h1 style={{color:"blueviolet"}}>Hello From Functional Component</h1>
+    }
+ export default fun ;
